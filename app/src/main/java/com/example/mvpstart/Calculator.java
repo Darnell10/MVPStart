115e1add8bc31a5a00c54309096a64b1d858764c
Darnell O <DarnellOtto@ac.c4q.nyc>
2019-08-19 09:17:56 -0400

    Calculator  method feeatureOne altered ,featureTwo created

--- app/src/main/java/com/example/mvpstart/Calculator.java
+++ app/src/main/java/com/example/mvpstart/Calculator.java
@@ -2,14 +2,22 @@ package com.example.mvpstart;
 
 public class Calculator {
 
-    public int featureOne (String input){
+    public int featureOne(String input) {
+
+        if (!input.matches("[0-9]+")) {
+            return 0;
+        }
+
         int result = 0;
         int inputNumber = Integer.parseInt(input);
-        result = inputNumber *10 - 6/3 %2;
+        result = inputNumber * 10 - 6 / 3 % 2;
         return result;
     }
 
 
+    public int featureTwo(String input) {
+        return 100;
+    }
 
 
 }
